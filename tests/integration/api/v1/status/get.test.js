b52c90050d5cf23c96176bf79f020ddb84974a97
André Freitas <dre.freitas23@gmail.com>
2024-10-13 03:03:31 -0300

    add a more robust ssl configuration method

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,5 +9,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.4");
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
