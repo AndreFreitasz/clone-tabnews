317f0f719f710eacef04ddd010607be96db5d568
AndreFreitasz <97642767+AndreFreitasz@users.noreply.github.com>
2025-02-11 01:41:11 +0000

    feat: adds `//api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -23,7 +23,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
   });
 
   await client.connect();
@@ -34,13 +34,3 @@ export default {
   query,
   getNewClient,
 };
-
-function getSSLValues() {
-  if (process.env.POSTGRES_CA) {
-    return {
-      ca: process.env.POSTGRES_CA,
-    };
-  }
-
-  return process.env.NODE_ENV === "development" ? false : true;
-}
